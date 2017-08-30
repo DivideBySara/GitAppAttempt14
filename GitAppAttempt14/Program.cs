b6ce778b7e6016eafbd05aa015ab4e6ae6ffa83a
dividebysara <s.jade@bellevuecollege.edu>
2017-08-30 11:28:20 -0700

    Test push VS code with README.md installed.

--- GitAppAttempt14/Program.cs
+++ GitAppAttempt14/Program.cs
@@ -13,6 +13,8 @@ namespace GitAppAttempt14
             Console.WriteLine("GitApp Attempt 14");
             Console.WriteLine("I can push code from the command line.");
             Console.WriteLine("Can I push code from the VS GUI?");
+            Console.WriteLine("I sure can! And I can pull a README.md file from GitHub");
+            Console.WriteLine("Now that I have a README.md, can I push more code to GitHub?");
             Console.ReadKey();
         }
     }
